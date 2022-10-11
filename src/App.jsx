c4ad249c6b3e7c956f821ebf780dbc57a36a1817
Anil Pujari <pujarianil4@gmail.com>
2022-10-11 11:00:50 +0530

    app with width

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useEffect } from 'react'
 import  './App.scss'
-import HallOfPools from './components/hallOfPools'
 import { setTheme } from './store/Action'
 import MainRoutes from './routes';
 import { useDispatch, useSelector } from 'react-redux';
 
 
 function App() {
-  // const [theme, setTheme] = useState('light');
   const dispatch = useDispatch();
- const themes = useSelector((state)=> state.theme)
- 
+  const themes = useSelector((state)=> state.theme)
+
   const handleTheme = () => {
     dispatch(setTheme(themes == 'dark'? 'light': 'dark'))
   }
