22180268531c1cfeaaf72f11da02c104c003d9b9
shubham <shubs12@gmail.com>
2022-11-23 18:14:23 +0530

    coinbase wallet added

--- src/constants/wallet.js
+++ src/constants/wallet.js
@@ -1,9 +1,15 @@
 // @dev provider options for multiple wallets
-import WalletConnectProvider from '@walletconnect/web3-provider/dist/umd/index.min.js';
-
-console.log(import.meta.env.REACT_APP_INFURA_ID);
+import WalletConnectProvider from '@walletconnect/web3-provider';
+import CoinbaseWalletSDK from '@coinbase/wallet-sdk';
 
 export const providerOptions = {
+  coinbasewallet: {
+    package: CoinbaseWalletSDK,
+    options: {
+      appName: 'unilend Finance',
+      infuraId: import.meta.env.REACT_APP_INFURA_ID,
+    },
+  },
   walletconnect: {
     package: WalletConnectProvider,
     options: {
