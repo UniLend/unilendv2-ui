505767e01611c9c18aa0dc826d6734096ae875af
Anil Pujari <anilpujari@Anils-MacBook-Air.local>
2023-07-04 17:49:31 +0530

    code refactor

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,28 @@
-import React, { Suspense } from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import { store } from './store/Store';
-import './index.css';
-import Ring from './components/Loader/Ring';
-  //uri: "https://api.thegraph.com/subgraphs/name/shubham-rathod1/unilend_mumbai",
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import { store } from "./store/Store";
+import "./index.css";
+import Ring from "./components/Loader/Ring";
+//uri: "https://api.thegraph.com/subgraphs/name/shubham-rathod1/unilend_mumbai",
 const client = new ApolloClient({
   uri: "https://api.thegraph.com/subgraphs/name/shubham-rathod1/my_unilend",
-  cache: new InMemoryCache()
+  cache: new InMemoryCache(),
 });
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-     <ApolloProvider client={client}>
-    <Suspense fallback={<Ring />}>
-    <Provider store={store} >
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-    </Provider>
-    </Suspense>
+    <ApolloProvider client={client}>
+      <Suspense fallback={<Ring />}>
+        <Provider store={store}>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </Provider>
+      </Suspense>
     </ApolloProvider>
   </React.StrictMode>
 );
