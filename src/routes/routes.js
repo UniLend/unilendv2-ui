d6afd5b563505e51fbeb5166341765689d72d18b
DAppArtisan <sandeep@unilend.finance>
2024-08-30 11:52:26 +0530

    update routes
    hide routes from live

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -21,24 +21,24 @@ export const openRoutes = [
     exact: true,
     element: lazy(() => import('../pages/pool')),
   },
-  {
-    path: "/dashboard",
-    exact: true,
-    element: lazy(() => import("../pages/userDashboard")),
-  },
+  // {
+  //   path: "/dashboard",
+  //   exact: true,
+  //   element: lazy(() => import("../pages/userDashboard")),
+  // },
   {
     path: '/governance',
     exact: true,
     element: lazy(() => import('../pages/vote')),
   },
-  {
-    path: '/vote',
-    exact: true,
-    element: lazy(() => import('../pages/governance')),
-  },
-  {
-    path: '/vote/:proposalId',
-    exact: true,
-    element: lazy(() => import('../pages/proposal')),
-  },
+  // {
+  //   path: '/vote',
+  //   exact: true,
+  //   element: lazy(() => import('../pages/governance')),
+  // },
+  // {
+  //   path: '/vote/:proposalId',
+  //   exact: true,
+  //   element: lazy(() => import('../pages/proposal')),
+  // },
 ];
